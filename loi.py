9ed4df76795396a7c0546d1ae85e7d61f3685e12
mayasoleimani <82066258+mayasoleimani@users.noreply.github.com>
2023-06-26 12:22:17 -0400

    Complete Dictionary

--- loi.py
+++ loi.py
@@ -1,7 +1,11 @@
 import re
 import numpy as np
-from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib as plt
+
+
 sia=SentimentIntensityAnalyzer()
+
 
 #                   Needed:                 #
 #init with variables {EntryID,Entry,WordCount,Date,StartTime,TotalTime,Compound}
@@ -42,14 +46,29 @@ class DiaryClassifiers(object):
             
             for x in grandjournal:
                 
-                sent=sia.polarity_scores(x)
+                entry=x[16:-5]
+                started=x[11:15]
+                wc=len(x[16:-5].split())
+
+#               no duration recorded
+                if x[11:15]!='0000' and x[len(x)-5:-1]!='0000':
+                    
+                    ended=x[len(x)-5:-1]
+                    tot=int(ended)-int(started)
+                
+                else:
+#                   error number for lack of total time
+                    tot='5555'
+
+                
+                sentiment_score=sia.polarity_scores(entry)
 
                 self.diary[self.entry_id]['entry']=x[16:-5]
-                self.diary[self.entry_id]['word count']=0
+                self.diary[self.entry_id]['word count']=wc
                 self.diary[self.entry_id]['date']=x[:11]
-                self.diary[self.entry_id]['start_time']=x[11:15]
-                self.diary[self.entry_id]['total_time']=0
-                self.diary[self.entry_id]['compound']=sent['compound']
+                self.diary[self.entry_id]['start_time']=started
+                self.diary[self.entry_id]['total_time']=tot
+                self.diary[self.entry_id]['compound']=sentiment_score['compound']
 
 
                 self.entry_id=self.entry_id+1
@@ -68,6 +87,16 @@ class DiaryClassifiers(object):
 
         pass
 
+    def InvertedSearch(self):
+        
+        print("Welcome to the LOI Sentiment Analyzer")
+        print("\n { All-time, Year range (2012-2022), Season, Word Count, Diary Length } \n")
+        independent=input("Please choose an Independent variable from the above list: ")
+
+        pass
+
+
+
 
             
 def main():
@@ -75,6 +104,10 @@ def main():
     main_run=DiaryClassifiers()
 
     main_run.DiaryAssignment()
+    main_run.InvertedSearch()
+
+    print("me")
+
 
 if __name__=='__main__':
     main()
